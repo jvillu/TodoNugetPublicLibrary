2549565d8e5ec625a8e1e24d86ea2d83c468a78b
JAVIER VILLULLAS <francisco.villullas@tokiota.com>
2023-01-20 10:27:53 +0100

    Inicio

---
+++ TodoPublicNuget.cs
@@ -0,0 +1,15 @@
+﻿namespace TodoNugetPublicLibrary
+{
+    public class TodoPublicNuget
+    {
+        public TodoPublicNuget()
+        {
+
+        }
+        public string GetTodo()
+        {
+            return "Creado por Public NUGET TodoNugetPublicLibrary";
+        }
+
+    }
+}
